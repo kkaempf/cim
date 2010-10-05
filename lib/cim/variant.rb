65ea152cb25ebd57dd478b57a9c102ff939d4d63
Klaus Kämpf <kkaempf@suse.de>
2010-10-05 11:47:50 +0200

    Variant: == operator added

--- lib/cim/variant.rb
+++ lib/cim/variant.rb
@@ -6,6 +6,19 @@ module CIM
       @type = (type.kind_of? CIM::Type) ? type : CIM::Type.new(type)
       @value = value unless value == :null
     end
+    def == v
+      $stderr.puts "<#{@type}>#{self} == #{v.class}"
+      case v
+      when NilClass:   @type == :null && @value.nil?
+      when FalseClass: @type == :bool && !@value
+      when TrueClass:  @type == :bool && @value
+      when String:     @type == :string && @value == v
+      when Integer:    @type == :int && @value == v
+      when Float:      @type == :real && @value == v
+      else
+	false
+      end			
+    end
     def to_s
       if @type == :null
 	"null"
