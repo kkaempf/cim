4528b7f63bbfae859310a995aa1633ace7a8b17a
Klaus Kämpf <kkaempf@suse.de>
2010-10-14 20:46:05 +0200

    Fixes for rake gem

--- lib/cim.rb
+++ lib/cim.rb
@@ -41,7 +41,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 #
 
 module CIM
-  VERSION = '0.3.1'
+  VERSION = '0.4.0'
   require 'cim/type'
   require 'cim/variant'
   require 'cim/qualifier_flavors'
